35acbeae76f1a19163f1d7ce3165667595187395
tmiles3 <thomasmiles44@gmail.com>
2020-11-30 20:16:06 -0700

    contact error

--- src/Components/Testimonials.js
+++ src/Components/Testimonials.js
@@ -4,14 +4,7 @@ class Testimonials extends Component {
   render() {
 
     if(this.props.data){
-      var testimonials = this.props.data.testimonials.map(function(testimonials){
-        return  <li key={testimonials.user}>
-            <blockquote>
-               <p>{testimonials.text}</p>
-               <cite>{testimonials.user}</cite>
-            </blockquote>
-         </li>
-      })
+
     }
 
     return (
